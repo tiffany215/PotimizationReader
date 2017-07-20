4119ac334bed960b78e5a3a8345c05fbbbbd2b6a
wangxu <tiffany_215@163.com>
2017-07-20 17:25:43 +0800

    调试获取书籍内容部分

--- readerCore/src/main/java/shuhai/readercore/Constants.java
+++ readerCore/src/main/java/shuhai/readercore/Constants.java
@@ -6,10 +6,6 @@ package shuhai.readercore;
 
 public class Constants {
 
-
-
-
-
     public static final long MAX_DISK_CACHE_SIZE = 10 * 1024 * 1024;
 
     public static final int MAX_MEMORY_CACHE_SIZE = (int) (Runtime.getRuntime().maxMemory() / 4);
@@ -21,7 +17,7 @@ public class Constants {
 
 
     public static final int MARGIN_WIDTH = 40;
-    public static final int MARGIN_HEIGTH = 40;
+    public static final int MARGIN_HEIGHT = 40;
 
 
 
